8ae567e14a1f416ddf473630f8087d5f2dce2c3c
notivri <katsu4973@gmail.com>
2025-10-17 00:06:30 +0700

    Add endOfLine auto

--- eslint.config.js
+++ eslint.config.js
@@ -27,8 +27,7 @@ export default defineConfig([
   },
   {
     rules: {
-      // override/add rules settings here, such as:
-      // 'vue/no-unused-vars': 'error'
+      endOfLine: "auto",
     },
     languageOptions: {
       sourceType: "module",
